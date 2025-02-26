df295e8afbccf91a045110b883c8f241828fd2f4
Shchuda <137898720+Shchuda@users.noreply.github.com>
2025-02-26 18:24:33 +0300

    Update book.py

--- book.py
+++ book.py
@@ -1,14 +1,25 @@
 class Book:
     def __init__(self, title, author, ISBN, copies_available):
+        """
+        Initializes a book with title, author, ISBN, and available copies.
+        """
         self.title = title
         self.author = author
         self.ISBN = ISBN
         self.copies_available = copies_available
 
     def is_available(self):
+        """
+        Checks if the book is available for borrowing.
+        Returns True if at least one copy is available, otherwise False.
+        """
         return self.copies_available > 0
 
     def borrow_book(self, member):
+        """
+        Allows a member to borrow the book if copies are available.
+        Reduces available copies by 1 if successful.
+        """
         if self.is_available():
             self.copies_available -= 1
             print(f"\n{member.name} (member ID: {member.member_id}) has borrowed the book '{self.title}' ✅")
@@ -18,6 +29,10 @@ class Book:
             return False
 
     def return_book(self):
+        """
+        Allows a member to return the book.
+        Increases available copies by 1 upon return.
+        """
         self.copies_available += 1
         print(f"\nYou have returned the book '{self.title}' 🔄✅")
         return True
